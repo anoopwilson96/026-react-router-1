2f03b025a784533d3ea74c18b6a30b55bf149cae
Anoop Wilson <anoopwilson33@yahoo.com>
2024-04-19 12:49:18 +0530

    installed react router

---
+++ src/routes/root.jsx
@@ -0,0 +1,22 @@
+export default function Root(){
+  return(
+<>
+<header>
+<div className="container" id="logoText">
+  <div>
+    <h2>Top 10 Cities</h2>
+    <p>And its weather</p>
+  </div>
+  <img id="logo" src="./image/logo.png" alt />
+</div>
+</header>
+<footer></footer>
+
+
+
+
+
+
+</>
+)
+}
